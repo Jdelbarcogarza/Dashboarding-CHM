bafc2e86b41e6a64fa427d04a8f793fa77daf326
Jorge Del Barco <jdelbarcogarza@gmail.com>
2022-04-02 20:03:57 -0600

    made whole component a clickable link

--- components/NavItem.jsx
+++ components/NavItem.jsx
@@ -2,39 +2,39 @@ import React from 'react'
 import { NextLink } from 'next/Link'
 import {
     Link,
-    Container,
-    Box
+    Box,
+    Typography
 } from '@mui/material'
 import AddLocationAltIcon from '@mui/icons-material/AddLocationAlt';
 
 export default function NavItem() {
     return (
         <>
-            <Box my={1} padding={1} sx={{
-                justifyContent: 'flex-start',
-                alignItems: 'center',
-                backgroundColor: 'skyblue',
-                display: 'flex',
-                borderColor: 'black',
-                borderStyle: 'solid',
-                borderRadius: '5px'
-            }}>
-             {/** Aqui va a ir el icono especifico para cada seccion de nuestra pagina */}
-                <AddLocationAltIcon />
-
-                <Link pl={2}
-                    sx={{
-                        width: '100%',
-                    }}
+            <Link
+                sx={{
+                    width: '100%',
+                }}
                 /** El componente link hereda la funcionalidad de NextLink con el prop 'component'*/
-                    component={NextLink} 
-                    underline="none"
-                    color="black"
-                    href='/'>
-                    Nav item
-                </Link>
+                component={NextLink}
+                underline="none"
+                color="black"
+                href='/'>
+                <Box  padding={2} sx={{
+                    justifyContent: 'flex-start',
+                    alignItems: 'center',
+                    backgroundColor: 'skyblue',
+                    display: 'flex',
+                    // TODO: 1. On hover change color 2. cursor does not change.
+                    borderRadius: '5px'
+                }}>
+                    {/** Aqui va a ir el icono especifico para cada seccion de nuestra pagina */}
+                    <AddLocationAltIcon />
 
-            </Box>
+                    <Typography 
+                        variant='subtitle'
+                        ml={2}>Nav item</Typography>
+                </Box>
+            </Link>
         </>
     )
 }
