aef6c2ae65a3e62e21edb76881134efeb8548bab
Jorge Del Barco <jdelbarcogarza@gmail.com>
2022-04-03 00:24:13 -0600

    implemented grid layout for the content of the page

--- pages/admin/search.js
+++ pages/admin/search.js
@@ -2,23 +2,71 @@ import React from 'react'
 import {
     Grid,
     Paper,
-    Box
+    Box,
+    Button,
+    Container
 } from '@mui/material'
 import Sidebar from '../../components/Sidebar'
+import Topbar from '../../components/Topbar'
 
 export default function search() {
     return (
         <>
-            <Grid container spacing={2}>
+            <Grid container>
                 <Grid item xs={2}>
                     {/** AQUI VA EL SIDEBAR */}
-                   <Sidebar />
+                    <Sidebar />
                 </Grid>
 
                 <Grid item xs={10}>
-                    {/** AQUI VA EL CONTENIDO QUE SE IRÁ ACTUALIZANDO */}
-                   
+                    <Topbar titleText={'Realizar busqueda de un paciente'} />
+
+                    {/**Este contenedor de grid es en el cual se despliega todo el contenido de la pagina */}
+                    <Container>
+                    <Grid container spacing={2}>
+                        <Grid item xs={6}>
+                            <Paper sx={{
+                                bgcolor: 'red',
+
+                            }}> qp</Paper>
+                        </Grid>
+
+                        <Grid item xs={6}>
+                            <Paper sx={{
+                                bgcolor: 'green',
+
+                            }}> qp</Paper>
+                        </Grid>
+
+                        <Grid item xs={8}>
+                            <Paper sx={{
+                                bgcolor: 'gray',
+
+                            }}> qp</Paper>
+                        </Grid>
+
+                    </Grid>
+
+</Container>
+
+
+
                 </Grid>
+
+
+                {/** AQUI VA EL CONTENIDO QUE SE IRÁ ACTUALIZANDO */}
+                <Grid container>
+                    <Grid item xs={3}>
+
+
+                    </Grid>
+                </Grid>
+
+
+
+
+
+
 
             </Grid>
         </>
