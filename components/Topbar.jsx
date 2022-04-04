585c4134789c0ccc5f50d7db7536a786bce2311a
Jorge Del Barco <jdelbarcogarza@gmail.com>
2022-04-03 19:28:47 -0500

    added 1em of margin bottom to Topbar

--- components/Topbar.jsx
+++ components/Topbar.jsx
@@ -6,7 +6,9 @@ import Typography from '@mui/material/Typography';
 
 export default function DenseAppBar({ titleText }) {
   return (
-    <Box sx={{ flexGrow: 1 }}>
+    <Box sx={{ flexGrow: 1,
+      marginBottom: '1em',
+     }}>
       <AppBar position="sticky">
         <Toolbar variant="dense">
           <Typography variant="h6" color="inherit" component="div">
