4571708ffa303de03292349b5b9cd06253c404e0
Jorge Del Barco <jdelbarcogarza@gmail.com>
2022-04-16 15:34:43 -0500

    added permanent info panel for patients

--- pages/admin/search.js
+++ pages/admin/search.js
@@ -24,14 +24,17 @@ import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
 import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
 
 import { NextLink } from 'next/Link'
-import { Link,
-Grid,
-Container,
-TextField,
-Autocomplete,
-Stack,
-Switch,
-Button } from '@mui/material'
+import {
+  Link,
+  Grid,
+  Container,
+  TextField,
+  Autocomplete,
+  Stack,
+  Switch,
+  Button,
+  Paper
+} from '@mui/material'
 
 import { DataGrid } from '@mui/x-data-grid'
 
@@ -109,99 +112,99 @@ const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' 
 export default function MiniDrawer() {
 
   // LOGICA PROPIA DE LA PAGINA 
-//////////////////////////////////// Constantes useState ////////////////////////////////////
+  //////////////////////////////////// Constantes useState ////////////////////////////////////
 
-    // Valores para habilitar los switches
-    const [enableIdSearch, setEnableIdSearch] = useState(true);
+  // Valores para habilitar los switches
+  const [enableIdSearch, setEnableIdSearch] = useState(true);
 
-    // Valores de textFields
-    const [patientName, setPatientName] = useState("");
-    const [patientID, setpatientID] = useState('');
+  // Valores de textFields
+  const [patientName, setPatientName] = useState("");
+  const [patientID, setpatientID] = useState('');
 
-    // Valores de atributo
-    const [atributo, setAtributo] = useState('');
+  // Valores de atributo
+  const [atributo, setAtributo] = useState('');
 
-    // Resultado de query
-    const [patientData, setPatientData] = useState([]);
+  // Resultado de query
+  const [patientData, setPatientData] = useState([]);
 
-    ///////////////////////////// Funciones y Constantes handle /////////////////////////////
+  ///////////////////////////// Funciones y Constantes handle /////////////////////////////
 
-    function handleSwitchChange() {
-        setEnableIdSearch(!enableIdSearch)
+  function handleSwitchChange() {
+    setEnableIdSearch(!enableIdSearch)
+  }
+
+  const handleAtributoChange = (event, newAtributo) => {
+    setAtributo(newAtributo);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const nombre = { patientName };
+    const pacID = { patientID };
+    const habilitado = { enableIdSearch };
+    const a = { atributo };
+    const tests = "";
+
+    console.log(a.atributo.length);
+
+    if (a.atributo.length == 0) {
+      tests = "$"
+    }
+    else {
+      var i = 0; for (i = 0; i < a.atributo.length; i++) {
+        if (a.atributo.length >= 1) {
+          tests = tests + "!" + a.atributo[i].value;
+        }
+      }
     }
 
-    const handleAtributoChange = (event, newAtributo) => {
-        setAtributo(newAtributo);
-    };
+    console.log(tests);
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-
-        const nombre = { patientName };
-        const pacID = { patientID };
-        const habilitado = { enableIdSearch };
-        const a = { atributo };
-        const tests = "";
-
-        console.log(a.atributo.length);
-
-        if (a.atributo.length == 0) {
-            tests = "$"
-        }
-        else {
-            var i = 0; for (i = 0; i < a.atributo.length; i++) {
-                if (a.atributo.length >= 1) {
-                    tests = tests + "!" + a.atributo[i].value;
-                }
-            }
-        }
-
-        console.log(tests);
-
-        if (pacID.patientID != '' & !habilitado.enableIdSearch) {
-            //console.log(pacID);
-            const resID = await fetch('/api/searchPrueba/one/ID/' + tests + '/' + pacID.patientID, { method: 'GET' }).then(resID => resID.json())
-            //console.log("Esto es resID");
-            //console.log(resID);
-            setPatientData(resID);
-        }
-        if (nombre.patientName != '' & habilitado.enableIdSearch) {
-            //console.log(nombre);
-            const resNom = await fetch('/api/searchPrueba/one/Name/' + tests + '/' + nombre.patientName, { method: 'GET' }).then(resNom => resNom.json())
-            //console.log("Esto es resNom");
-            //console.log(resNom);
-            setPatientData(resNom);
-            console.log(resNom[0].ID_Usuario)
-        }
+    if (pacID.patientID != '' & !habilitado.enableIdSearch) {
+      //console.log(pacID);
+      const resID = await fetch('/api/searchPrueba/one/ID/' + tests + '/' + pacID.patientID, { method: 'GET' }).then(resID => resID.json())
+      //console.log("Esto es resID");
+      //console.log(resID);
+      setPatientData(resID);
     }
+    if (nombre.patientName != '' & habilitado.enableIdSearch) {
+      //console.log(nombre);
+      const resNom = await fetch('/api/searchPrueba/one/Name/' + tests + '/' + nombre.patientName, { method: 'GET' }).then(resNom => resNom.json())
+      //console.log("Esto es resNom");
+      //console.log(resNom);
+      setPatientData(resNom);
+      console.log(resNom[0].ID_Usuario)
+    }
+  }
 
-    /////////////////////////////// COLUMNAS DEL DATAGRID /////////////////////////////////
+  /////////////////////////////// COLUMNAS DEL DATAGRID /////////////////////////////////
 
-    let columns = [
+  let columns = [
 
-        { field: "ID_Usuario" },
-        { field: "Nombre" },
-        { field: "Año_Nac" },
-        { field: "Genero" },
-        { field: "ID_Parroquia" },
-        { field: "ID_Resultado" },
-        { field: "Reloj" },
-        { field: "Orient_Temp" },
-        { field: "Orient_Esp" },
-        { field: "Registro" },
-        { field: "Calculo" },
-        { field: "Memoria" },
-        { field: "Eject" },
-        { field: "GDS_Total" },
-        { field: "Katz_Total" },
-        { field: "LWB_Total" },
-        { field: "Sarc_F" },
-        { field: "Fuerza_Domin" },
-        { field: "SPPB_Global" },
-        { field: "CFS_Fraility" },
-        { field: "Gijon" }
+    { field: "ID_Usuario" },
+    { field: "Nombre" },
+    { field: "Año_Nac" },
+    { field: "Genero" },
+    { field: "ID_Parroquia" },
+    { field: "ID_Resultado" },
+    { field: "Reloj" },
+    { field: "Orient_Temp" },
+    { field: "Orient_Esp" },
+    { field: "Registro" },
+    { field: "Calculo" },
+    { field: "Memoria" },
+    { field: "Eject" },
+    { field: "GDS_Total" },
+    { field: "Katz_Total" },
+    { field: "LWB_Total" },
+    { field: "Sarc_F" },
+    { field: "Fuerza_Domin" },
+    { field: "SPPB_Global" },
+    { field: "CFS_Fraility" },
+    { field: "Gijon" }
 
-    ]
+  ]
 
 
   const theme = useTheme();
@@ -221,10 +224,10 @@ export default function MiniDrawer() {
     { name: 'Realizar consulta', icon: <PersonSearchOutlinedIcon />, route: 'search' },
     { name: 'Cargar datos', icon: <CloudUploadOutlinedIcon />, route: 'loadData' },
     { name: 'Modificar datos', icon: <EditOutlinedIcon />, route: 'modifyData' },
-    { name: 'Cerrar sesión', icon: <LogoutOutlinedIcon />, route: '/'},
-];
+    { name: 'Cerrar sesión', icon: <LogoutOutlinedIcon />, route: '/' },
+  ];
 
-const appBarText = 'Realizar busqueda de pacientes'
+  const appBarText = 'Realizar busqueda de pacientes'
 
   return (
     <Box sx={{ display: 'flex' }}>
@@ -256,154 +259,176 @@ const appBarText = 'Realizar busqueda de pacientes'
         </DrawerHeader>
         <Divider />
         <List>
-        
+
           {sidebarElements.map((item, index) => (
             <Link
-            component={NextLink}
-            href={item.route}
-            underline={'none'}
-            key={index}
-            color={'gray'}>
-            {item.name === 'Cerrar sesión' ? <Divider key={'divider'} /> : null}
+              component={NextLink}
+              href={item.route}
+              underline={'none'}
+              key={index}
+              color={'gray'}>
+              {item.name === 'Cerrar sesión' ? <Divider key={'divider'} /> : null}
 
-                <ListItemButton
+              <ListItemButton
                 key={index}
                 sx={{
-                    minHeight: 48,
-                    justifyContent: open ? 'initial' : 'center',
-                    px: 2.5,
+                  minHeight: 48,
+                  justifyContent: open ? 'initial' : 'center',
+                  px: 2.5,
                 }}
-                >
+              >
 
                 <ListItemIcon
-                    
-                    sx={{
+
+                  sx={{
                     minWidth: 0,
                     mr: open ? 3 : 'auto',
                     justifyContent: 'center',
-                    }}
+                  }}
                 >
-                    {item.icon}
+                  {item.icon}
                 </ListItemIcon>
                 <ListItemText primary={item.name} sx={{ opacity: open ? 1 : 0 }} />
-                </ListItemButton>
+              </ListItemButton>
             </Link>
           ))}
 
         </List>
       </Drawer>
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-      <DrawerHeader />
+        <DrawerHeader />
 
-      {/** AQUI VA EL CODIGO DE CADA PAGINA */}
-      <Container>
-                        <Grid container spacing={2}>
+        {/** AQUI VA EL CODIGO DE CADA PAGINA */}
+        <Container>
+          <Grid container spacing={2}>
 
-                            <Grid item xs={12}>
-                                <Typography variant='h6'>Favor de llenar los campos como corresponde</Typography>
-                            </Grid>
+            <Grid item xs={12}>
+              <Typography variant='h6'>Favor de llenar los campos como corresponde</Typography>
+            </Grid>
 
-                            {/** SECCIÓN DE CAMPOS DE BÚSQUEDA */}
-                            <Grid item xs={6}>
-                                <Stack spacing={2}>
-                                    <Box sx={{
-                                        width: '60%'
-                                    }}>
-                                        <TextField
-                                            label='Nombre del paciente'
-                                            value={patientName}
-                                            onChange={(e) => { setPatientName(e.target.value) }}
-                                            variant='standard'
-                                            disabled={!enableIdSearch}
-                                            fullWidth />
-                                        {/*console.log("el valor es ", patientName)*/}
-                                    </Box>
+            {/** SECCIÓN DE CAMPOS DE BÚSQUEDA */}
+            <Grid item xs={6}>
+              <Stack spacing={2}>
+                <Box sx={{
+                  width: '60%'
+                }}>
+                  <TextField
+                    label='Nombre del paciente'
+                    value={patientName}
+                    onChange={(e) => { setPatientName(e.target.value) }}
+                    variant='standard'
+                    disabled={!enableIdSearch}
+                    fullWidth />
+                  {/*console.log("el valor es ", patientName)*/}
+                </Box>
 
-                                    <Box sx={{ display: 'flex', alignItems: 'baseline', width: '60%' }}>
-                                        <TextField
-                                            label='ID del paciente'
-                                            value={patientID}
-                                            onChange={(e) => { setpatientID(e.target.value) }}
-                                            variant='standard'
-                                            helperText={'La busqueda por ID desactivará la consulta por nombre'}
-                                            disabled={enableIdSearch} />
-                                        {/*console.log("el valor es ", patientID)*/}
-                                        <Switch onChange={handleSwitchChange} />
-                                    </Box>
-                                    <Box>
-                                        <Button variant='contained' onClick={handleSubmit}>Realizar busqueda</Button>
-                                    </Box>
-                                </Stack>
-                            </Grid>
+                <Box sx={{ display: 'flex', alignItems: 'baseline', width: '60%' }}>
+                  <TextField
+                    label='ID del paciente'
+                    value={patientID}
+                    onChange={(e) => { setpatientID(e.target.value) }}
+                    variant='standard'
+                    helperText={'La busqueda por ID desactivará la consulta por nombre'}
+                    disabled={enableIdSearch} />
+                  {/*console.log("el valor es ", patientID)*/}
+                  <Switch onChange={handleSwitchChange} />
+                </Box>
+                <Box>
+                  <Button variant='contained' onClick={handleSubmit}>Realizar busqueda</Button>
+                </Box>
+              </Stack>
+            </Grid>
 
-                            {/** PANEL DE INSTRUCCIONES Y DE INFO ADICIONAL */}
-                            <Stack spacing={5} sx={{ width: 400 }}>
-                                <Autocomplete
-                                    multiple
-                                    options={atributosPrueba}
-                                    groupBy={(option) => option.prueba}
-                                    getOptionLabel={(option) => option.atributo}
-                                    onChange={handleAtributoChange}
-                                    renderInput={(params) => (
-                                        <TextField
-                                            {...params}
-                                            variant="standard"
-                                            label="Seleccione los atributos de la prueba"
-                                            placeholder="Atributos"
-                                        />
-                                    )}
-                                />
+            {/** PANEL DE INSTRUCCIONES Y DE INFO ADICIONAL */}
 
-                                <Button href={'searchGroup'} variant='contained'>Busqueda General</Button>
-                            </Stack>
+            <Grid item xs={6}>
 
-                            {/** SECCIÓN DE RESULTADOS CON TABLA */}
-                            <Grid item xs={12}>
-
-                                <Divider />
-
-                                <Typography
-                                    variant='h6'
-                                    sx={{
-                                        marginY: '2em'
-                                    }}>Aqui se despliegan los resultados de la búsqueda?</Typography>
-                                
-                                    {/** AQUI VA EL DATAGRID */}
-                                    <DataGrid
-                                        getRowId={(id) => id.ID_Resultado} // Asigna que el id unico es el atributo ID_Usuario
-                                        columns={columns}
-                                        rows={patientData}
-                                        pageSize={5}
-                                        rowsPerPageOptions={[5]}
-                                    />
-
-                            </Grid>
-                        </Grid>
-                    </Container>
+                <Paper sx={{bgcolor: 'red'}} elevation={2}>
+                <Container>
+                  <Typography variant="subtitle1" color="initial">Datos del paciente</Typography>
+                  <Typography variant="body1" color="initial">ID De usuario: <em>{patientData[0].ID_Usuario}</em></Typography>
+                  <Typography variant="body1" color="initial">Nombre: <em>{patientData[0].Nombre}</em></Typography>
+                  <Typography variant="body1" color="initial">Año de nacimiento: <em>{patientData[0].Año_Nac}</em></Typography>
+                  <Typography variant="body1" color="initial">Genero: <em>{patientData[0].Genero == 1 ? 'Masculino' : 'Femenino'}</em></Typography>
 
 
 
-        </Box>
+                  </Container>
+                </Paper>
+
+            </Grid>
+
+            <Stack spacing={5} sx={{ width: 400 }}>
+              
+
+              
+              <Autocomplete
+                multiple
+                options={atributosPrueba}
+                groupBy={(option) => option.prueba}
+                getOptionLabel={(option) => option.atributo}
+                onChange={handleAtributoChange}
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    variant="standard"
+                    label="Seleccione los atributos de la prueba"
+                    placeholder="Atributos"
+                  />
+                )}
+              />
+
+              <Button href={'searchGroup'} variant='contained' color={'secondary'}>Busqueda General</Button>
+            </Stack>
+
+            {/** SECCIÓN DE RESULTADOS CON TABLA */}
+            <Grid item xs={12}>
+
+              <Divider />
+
+              <Typography
+                variant='h6'
+                sx={{
+                  marginY: '1em'
+                }}>Aqui se despliegan los resultados de la búsqueda</Typography>
+
+                
+
+              {/** AQUI VA EL DATAGRID */}
+              <DataGrid
+                getRowId={(id) => id.ID_Resultado} // Asigna que el id unico es el atributo ID_Usuario
+                columns={columns}
+                rows={patientData}
+                pageSize={5}
+                rowsPerPageOptions={[5]}
+                autoHeight
+              />
+
+            </Grid>
+          </Grid>
+
+        </Container>
+      </Box>
     </Box>
 
-      
+
   );
 }
 
 const atributosPrueba = [
-  { prueba: 'Reloj', atributo: 'Reloj', value: 'Reloj'},
-  { prueba: 'MMSE', atributo: 'Orientacion Temporal', value: 'Orient_Temp'},
-  { prueba: 'MMSE', atributo: 'Orientacion Espacial', value: 'Orient_Esp'},
-  { prueba: 'MMSE', atributo: 'Registro', value: 'Registro'},
-  { prueba: 'MMSE', atributo: 'Calculo', value: 'Calculo'},
-  { prueba: 'MMSE', atributo: 'Memoria', value: 'Memoria'},
-  { prueba: 'MMSE', atributo: 'Eject', value: 'Eject'},
-  { prueba: 'GDS', atributo: 'GDS', value: 'GDS_Total'},
-  { prueba: 'Katz', atributo: 'Katz', value: 'Katz_Total'},
-  { prueba: 'LWB', atributo: 'LWB', value: 'LWB_Total'},
-  { prueba: 'Sarc F', atributo: 'Sarc F', value: 'Sarc_F'},
-  { prueba: 'Fuerza', atributo: 'Fuerza', value: 'Fuerza_Domin'},
-  { prueba: 'SPPB', atributo: 'SPPB', value: 'SPPB_Global'},
-  { prueba: 'CFS Fraility', atributo: 'CFS Fraility', value: 'CFS_Fraility'},
-  { prueba: 'Gijon', atributo: 'Gijon', value: 'Gijon'},
+  { prueba: 'Reloj', atributo: 'Reloj', value: 'Reloj' },
+  { prueba: 'MMSE', atributo: 'Orientacion Temporal', value: 'Orient_Temp' },
+  { prueba: 'MMSE', atributo: 'Orientacion Espacial', value: 'Orient_Esp' },
+  { prueba: 'MMSE', atributo: 'Registro', value: 'Registro' },
+  { prueba: 'MMSE', atributo: 'Calculo', value: 'Calculo' },
+  { prueba: 'MMSE', atributo: 'Memoria', value: 'Memoria' },
+  { prueba: 'MMSE', atributo: 'Eject', value: 'Eject' },
+  { prueba: 'GDS', atributo: 'GDS', value: 'GDS_Total' },
+  { prueba: 'Katz', atributo: 'Katz', value: 'Katz_Total' },
+  { prueba: 'LWB', atributo: 'LWB', value: 'LWB_Total' },
+  { prueba: 'Sarc F', atributo: 'Sarc F', value: 'Sarc_F' },
+  { prueba: 'Fuerza', atributo: 'Fuerza', value: 'Fuerza_Domin' },
+  { prueba: 'SPPB', atributo: 'SPPB', value: 'SPPB_Global' },
+  { prueba: 'CFS Fraility', atributo: 'CFS Fraility', value: 'CFS_Fraility' },
+  { prueba: 'Gijon', atributo: 'Gijon', value: 'Gijon' },
 ];
