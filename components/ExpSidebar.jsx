cc2fed778b60b554804f806294f9b43f60571cc4
Jorge Del Barco <jdelbarcogarza@gmail.com>
2022-04-16 20:29:25 -0500

    deleted unnecessary imports

--- components/ExpSidebar.jsx
+++ components/ExpSidebar.jsx
@@ -15,15 +15,12 @@ import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
 
 // mis imports
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 import PersonSearchOutlinedIcon from '@mui/icons-material/PersonSearchOutlined';
 import CloudUploadOutlinedIcon from '@mui/icons-material/CloudUploadOutlined';
 import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
-import ArrowBackOutlinedIcon from '@mui/icons-material/ArrowBackOutlined';
 import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
 
 import { Link } from '@mui/material'
