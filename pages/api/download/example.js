8c1adabe61d176d0dee1b82a49d196e92a240602
BrendaSaucedo12 <A00829855@itesm.mx>
2022-04-21 22:52:32 -0500

    loadData fixed

--- pages/api/download/example.js
+++ pages/api/download/example.js
@@ -1,14 +1,12 @@
 import fs from 'fs'
-import next from 'next'
 
 export default (req, res) => {
     // Modificar según la ruta de cada quien
     const ruta = "c:\\Users\\nenas\\OneDrive\\Documents\\GitHub\\C.MTY.TC2005B.413.2211.Equipo3\\public\\ExcelTemplate\\BaseDatos.xlsx"
-    const filename = "hola.xlsx"
-
+    
     fs.readFile(ruta, (err, data) => {
         if (err) {
-            return next(err)
+            return res.status(500).json({"msg":"Error downloading file"});
         }
         res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
         res.setHeader('Content-Disposition', 'attachment; filename=plantilla.xlsx')
