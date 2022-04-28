2510aa93e3dd096f4a297dc621199480f70105d4
Patricio Mendoza <>
2022-04-28 11:08:32 -0500

    checkpoint

--- pages/api/location/zona/[zona].js
+++ pages/api/location/zona/[zona].js
@@ -2,7 +2,7 @@ import { connection } from "../../../../config/db";
 
 export default async function handler(req, res) {
   const [rows] = await connection.query(
-    `SELECT Nombre FROM Zona`
+    `SELECT ID_Zona, Nombre FROM Zona`
   );
   return res.status(200).json(rows);
 }
